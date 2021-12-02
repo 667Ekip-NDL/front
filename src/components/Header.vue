9aea6b64725d0ca77f4ef58e86374b3354f63642
Djamel <djamelboulechfar@gmail.com>
2021-12-03 00:59:53 +0100

    Début page d'accueil

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="home__header">
-      <span class="home__header__title">Sauveteurs du dunkerquois</span>
-      <ul class="home__header__links">
+    <div class="header">
+      <span class="header__title">Sauveteurs du dunkerquois</span>
+      <ul class="header__links">
         <li>Accueil</li>
       </ul>
-      <span class="home__header__spacer"></span>
-      <span class="home__header__searchText">Rechercher :</span>
-      <input id="searchbar" type="text" placeholder="Sauveteurs, bateaux, ..." class="home__header__searchInput" v-model="input">
-      <img src="../images/loupe.png" class="home__header__searchIcon">      
+      <span class="header__spacer"></span>
+      <span class="header__searchText">Rechercher :</span>
+      <input id="searchbar" type="text" placeholder="Sauveteurs, bateaux, ..." class="header__searchInput" v-model="input">
+      <img src="../images/loupe.png" class="header__searchIcon">      
     </div>
     <div>
         <div v-for="r in search_results" v-bind:key="r">
@@ -37,7 +37,7 @@ export default{
             sauveteur:false,
             bateau:false,
             search_results:[
-                {"type":"bateau",
+                /*{"type":"bateau",
                 "data":{
                     "nom":"tom",
                     "capacite":"5",
@@ -54,9 +54,7 @@ export default{
                     "capitaine":"pas bob l'eponge",
                     "sorties":"10",
                     }
-                },
-                
-
+                }*/
             ]
         }
     },
@@ -76,64 +74,57 @@ export default{
 }
 </script>
 <style lang="scss">
-.home {
+.header {
+  display: flex;
+  align-items: center;
   width: 100%;
-  height: 100%;
-  font-family: 'BioRhyme', serif;
-  background-color: rgb(250, 250, 250);
+  height: 64px;
+  color: white;
+  background-color: navy;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.25);
 
-  &__header {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: 64px;
-    color: white;
-    background-color: navy;
-    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.25);
+  &__title {
+    margin-left: 16px;
+    font-size: 32px;
+    font-weight: bold;
+  }
 
-    &__title {
-      margin-left: 16px;
-      font-size: 32px;
-      font-weight: bold;
-    }
+  &__links {
+    display: inline;
+    margin-left: 32px;
+    padding-left: 0px;
+    font-size: 24px;
+    list-style: none;
 
-    &__links {
-      display: inline;
-      margin-left: 32px;
-      padding-left: 0px;
-      font-size: 24px;
-      list-style: none;
-
-      li {
-        cursor: pointer;
-      }
-    }
-
-    &__spacer {
-      flex: 1 1 auto;
-    }
-
-    &__searchText {
-      margin-right: 8px;
-      font-size: 24px;
-    }
-
-    &__searchInput {
-      padding: 2px 0px 2px 4px;
-      height: 30px;
-      background-color: white;
-      border: none;
-      border-radius: 15px 0px 0px 15px;
-    }
-
-    &__searchIcon {
-      margin-right: 16px;
-      padding: 2px 4px 2px 4px;
-      height: 30px;
-      background-color: white;
-      border-radius: 0px 15px 15px 0px;
+    li {
       cursor: pointer;
     }
+  }
+
+  &__spacer {
+    flex: 1 1 auto;
+  }
+
+  &__searchText {
+    margin-right: 8px;
+    font-size: 24px;
+  }
+
+  &__searchInput {
+    padding: 2px 0px 2px 4px;
+    height: 30px;
+    background-color: white;
+    border: none;
+    border-radius: 15px 0px 0px 15px;
+  }
+
+  &__searchIcon {
+    margin-right: 16px;
+    padding: 2px 4px 2px 4px;
+    height: 30px;
+    background-color: white;
+    border-radius: 0px 15px 15px 0px;
+    cursor: pointer;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,79 @@
 <template>
   <div class="home">
-    <!--<HelloWorld msg="Template par Heargo"/>-->
     <Header/>   
-    <div>
-      <h1>Test sauveteur</h1>
-      <FicheTechniqueSauveteur/>
-      <h1>Test bateaux</h1>
-      <Bateau></Bateau>
-      <Bateau></Bateau>
+    <div class="home__content">
+      <div class="home__content__bienvenue">
+        <div class="home__content__bienvenue__imageAccueil"></div>
+        <h2 class="home__content__bienvenue__texteAccueil">
+          Bienvenue sur le site des sauveteurs du dunkerquois.
+          <br><br>
+          Ce site rend hommage aux femmes, hommes et enfants qui ont réalisé des actes de sauvetages en milieu aquatique.
+          <br><br>
+          Ces sauveteurs, habitants du dunkerquois (de Bray-Dunes à Grand-Fort-Philippe), 
+          ont participé à plus de 900 sauvetages en mer et plus de 1100 sauvetages individuels.
+          <br><br>
+          Œuvrant avec courage, abnégation et souvent au mépris du risque ils méritent amplement que leurs actes soient pérennisés. 
+        </h2>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
-import FicheTechniqueSauveteur from '../components/FicheTechniqueSauveteur.vue'
-import Bateau from '@/components/Bateau.vue'
 import Header from '@/components/Header.vue'
 
 export default {
   name: 'Home',
   components: {
-    //HelloWorld,
-    FicheTechniqueSauveteur,
-    Bateau,
     Header
   }
 }
 </script>
-<style lang="scss">
 
+<style lang="scss">
+.home {
+  width: 100%;
+  min-height: 100vh;
+  background-color: rgb(250, 250, 250);
+  
+  &__content {
+    border-bottom: solid 5px navy;
+
+    &__bienvenue {
+      position: relative;
+      width: 100%;
+      height: 700px;
+
+      &__imageAccueil {
+        position: absolute;
+        z-index: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url(../images/dunkerque.webp);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: blur(4px);
+      }
+
+      &__texteAccueil {
+        position: absolute;
+        z-index: 1;
+        top: 80px;
+        left: 100px;
+        margin: 0px;
+        padding: 16px;
+        width: 60%;
+        color: white;
+        font-size: 36px;
+        font-weight: bold;
+        text-align: justify;
+        background-color: rgba(0, 0, 0, 0.5);
+        border: solid 3px white;
+        box-shadow: 0px 0px 100px rgba(0, 0, 0, 0.5);
+      }
+    }
+  }
+}
 </style>
