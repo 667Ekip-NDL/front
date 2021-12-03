5a92140b2650093d839d250ff290d5b019e9d7d6
Hearstgo <hrey222@gmail.com>
2021-12-03 02:57:34 +0100

    ajout design add page

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
+    <div class="big_container">
+        <router-link to="/" class="homebtn"><img src="@/assets/boat.png" alt="home">Home</router-link>
+        <img class="bg" src="@/assets/svg/bg_add.svg" alt="bg">
+        <img class="hand" src="@/assets/svg/hand.svg" alt="hand">
         <div class="flex" v-show="!personOn && !sauvetageOn">
             <p class="options" @click="this.personOn=true">Sauveteur</p>
             <p class="options" @click="this.sauvetageOn=true">Sauvetage</p>
@@ -7,19 +10,19 @@
         <div class="flex" v-show="personOn">
             <img @click="this.personOn=false" class="goback" src="@/assets/back.png" alt="">
             <form>
-                <input type="text" placeholder="Nom">
-                <input type="text" placeholder="prénom">
+                <input type="text" placeholder="NOM">
+                <input type="text" placeholder="Prénom">
                 <input type="submit">
             </form>
         </div>
         <div class="flex" v-show="sauvetageOn">
             <img @click="this.sauvetageOn=false" class="goback" src="@/assets/back.png" alt="">
             <form >
-                <input type="text" placeholder="titre">
-                <input type="text" placeholder="date">
-                <input type="text" placeholder="personnes sauvées">
+                <input type="text" placeholder="Titre">
+                <input type="text" placeholder="Date">
+                <input type="text" placeholder="Personnes sauvées">
                 <p>Séparez les participants par des virgules (,)</p>
-                <input type="text" placeholder="participants">
+                <input type="text" placeholder="Participants">
 
                 <input type="submit">
             </form>
@@ -41,7 +44,21 @@ export default {
 }
 </script>
 <style lang="scss">
-
+    .big_container{
+        overflow: hidden
+    }
+    .bg{
+        background-size: cover;
+        background-repeat: no-repeat;
+        width: 100vw;
+        height: 100vh;
+    }
+    .hand{
+        position: absolute;
+        left: 30%;
+        transform: scale(1.7);
+        z-index: -2;
+    }
     .flex{
         width: 100%;
         height: 100vh;
@@ -86,6 +103,27 @@ export default {
             height:50px;
             margin:0.5rem 0;
             font-size:1.5rem;
+
+            &[type='submit']:hover{
+                background-color:$dark-blue;
+                color:$white;
+            }
         }
+    }
+    .homebtn{
+        position:absolute;
+        top: 10px;
+        left: 10px;
+        img{
+            width:5vh;
+            height:5vh;
+            margin-right: 5px;
+        }
+        display:flex;
+        align-items:center;
+        font-size:2rem;
+        color:$dark-blue;
+        text-decoration: none;
+        
     }
 </style>
