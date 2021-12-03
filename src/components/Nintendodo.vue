510b8ace53589984ff1a69ae7811112019fcd82f
Léo <58537513+leolaf@users.noreply.github.com>
2021-12-03 06:27:24 +0100

    pub jusqu'à 100 moutons

---
+++ src/components/Nintendodo.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class ="nintendodo-div">
+		<div class="div-center">
+			<img src="@/images/nintendodo.gif" alt="Pub console Ubisoft">
+		</div>
+		<div class="nb-moutons" id="countSheep">
+			0
+		</div>
+	</div>
+</template>
+
+<script>
+	var sheepCounter = 0
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms))
+	}
+
+	function initializeCounter(){
+		sleep(1300).then(() => {
+			incrementSheepCounter()
+		})
+	}
+
+	function incrementSheepCounter(){
+		if(sheepCounter<100){
+			sheepCounter++
+			document.getElementById("countSheep").innerHTML = sheepCounter
+			sleep(1750)
+				.then(() => {incrementSheepCounter()})
+		}
+	}
+	initializeCounter()
+</script>
+
+<style lang="scss">
+.div-center
+{
+	width:300px;
+	margin : auto;
+}
+.nb-moutons
+{
+	position : absolute;
+	left:calc(50% - 140px);
+	bottom: 18px;
+	font-size : 30pt;
+	color : blue;
+}
+.nintendodo-div
+{
+	margin-top : 20px;
+	margin-bottom : 20px;
+	position:relative;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -60,23 +60,26 @@
       <img  src="@/assets/load.gif" alt="thomas pesquet">
     </div>
   <!--cards-->
-  <div v-show="searched && research_response!={}">
-    <div>
-        <div v-for="r in research_response" v-bind:key="r">
-            <Bateau v-if="r['type']=='sauvetage'"
-            :title="r['data']['title']"
-            :date="r['data']['date']"
-            :personnes_sauves="r['data']['saved']"
-            :participants="r['data']['participants']"
-            />
-            <FicheTechniqueSauveteur v-if="r['type']=='personne'"
-            :nom_sauveteur="r['data']['nom']"
-            :prenom_sauveteur="r['data']['prenom']"
-            :personnes_sauves="r['data']['saved']"
-            />
-        </div>
+    <div v-show="searched && research_response!={}">
+      <div>
+          <div v-for="r in research_response" v-bind:key="r">
+              <Bateau v-if="r['type']=='sauvetage'"
+              :title="r['data']['title']"
+              :date="r['data']['date']"
+              :personnes_sauves="r['data']['saved']"
+              :participants="r['data']['participants']"
+              />
+              <FicheTechniqueSauveteur v-if="r['type']=='personne'"
+              :nom_sauveteur="r['data']['nom']"
+              :prenom_sauveteur="r['data']['prenom']"
+              :personnes_sauves="r['data']['saved']"
+              />
+          </div>
+      </div>
     </div>
-  </div>
+    <div>
+      <Nintendodo/>
+    </div>
   </div>
 </template>
 
@@ -85,12 +88,13 @@
 import Header from '@/components/Header.vue'
 import Bateau from '@/components/Bateau.vue'
 import FicheTechniqueSauveteur from '@/components/FicheTechniqueSauveteur.vue'
+import Nintendodo from "@/components/Nintendodo.vue"
 import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
-    Header,Bateau,FicheTechniqueSauveteur
+    Header,Bateau,FicheTechniqueSauveteur,Nintendodo
   },
   computed: {
         ...mapState(['research_response','searched','searchedDone'])
